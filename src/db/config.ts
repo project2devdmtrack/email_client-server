80b2e42321b402906f5d21299295b85569ba1c13
dmtrack <rightrack888@gmail.com>
2023-01-25 20:10:29 +0500

    add: env

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from 'sequelize-typescript';
 import { User } from '../models/users';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connection = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'root',
-    database: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     logging: false,
     models: [User],
 });
